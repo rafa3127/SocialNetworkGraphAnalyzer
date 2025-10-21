6d8f74a8cf40d1c0401dbd7f30bf77ee98e408e1
Rafael Correa <rafaelc3127@MNB526.teaminternational.com>
2025-10-21 19:12:16 -0400

    Initial commit: initialize java application project

---
+++ src/main/java/com/graph/socialnetworkgraphanalyzer/SocialNetworkGraphAnalyzer.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.graph.socialnetworkgraphanalyzer;
+
+/**
+ *
+ * @author rafaelc3127
+ */
+public class SocialNetworkGraphAnalyzer {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
